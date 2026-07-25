41f8e56958184ff3e130bce838176efbb28b48e4
ReTr0777 <irwangorter@hotmail.com>
2026-07-25 18:39:00 +0200

    Add project source files and configuration

---
+++ src/client/java/com/retr0/armor/toolsmiths/overhaul/client/ArmorToolsmithsOverhaulClient.java
@@ -0,0 +1,10 @@
+package com.retr0.armor.toolsmiths.overhaul.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ArmorToolsmithsOverhaulClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
